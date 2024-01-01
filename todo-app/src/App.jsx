6cc5d01a65fcfbba20718ac129dc902543ad99d1
umair kifayat <umairkifayat3@gmail.com>
2023-12-31 23:51:37 -0800

    'updated'

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 
 
 function App() {
-  const [text, setText] = useState("")
+  const [text, setText] = useState("");
+  const [todo, setTodo] = useState([]);
+
 
 function addtodo(event) {
   event.preventDefault()
-  console.log(text);
-  
+  todo.push(text);
+  setTodo([...todo])
+  console.log(todo);
+
 }
 
 
@@ -16,9 +20,17 @@ function addtodo(event) {
     <>
     <h1>Todo App</h1>
     <form onSubmit={addtodo}>
-      <input type="text" onChange={(e)=> setText(e.target.value)} />
-      <button>Add</button>
+      <input type="text" onChange={(e)=> setText(e.target.value)} value={text}/>
+      <button type='submit'>Add</button>
     </form>
+    <ul>
+      {todo.map((item,index)=>{
+return <li key={index}>{item}
+<button>Delete</button>
+<button>edit</button>
+</li>
+})}
+    </ul>
     </>
   )
 }
