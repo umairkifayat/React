921e8eb38ad8d0351dc91f5c678feb0b48331506
umair kifayat <umairkifayat3@gmail.com>
2024-01-07 14:03:14 -0800

    'haif'

--- generator-password/src/App.jsx
+++ generator-password/src/App.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopy } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   const [password, setPassword] = useState('');
   const [length, setLength] = useState(5);
-  const [symbol, setSymbol] = useState(true);
-  const [number, setNumber] = useState(true);
+  const [symbol, setSymbol] = useState(false);
+  const [number, setNumber] = useState(false);
 
   useEffect(() => {
     generatePassword();
@@ -17,6 +19,13 @@ function App() {
   function includeSymbol(e) {
     setSymbol(e.target.checked);
   }
+
+  function copyText() {
+    navigator.clipboard.writeText(password)
+  }
+
+
+
 
   function generatePassword() {
     let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
@@ -34,10 +43,25 @@ function App() {
     }
     setPassword(generatedPassword);
   }
+  
 
   return (
+
     <>
-      <h1>{password}</h1>
+   <center >
+                <h1 className=' font-serif text-5xl mt-[6%] bg-[#e6ebef] text-[#175ddc] p-[4%]
+   w-[80%]  rounded-[15px]'>{password}</h1>
+                </center>
+
+         <center>             
+     <button onClick={ copyText} className='bg-[#020f66] text-white mt-5 p-[1%] rounded-[15px]'><FontAwesomeIcon className='text-[gray] rounded-2xl' icon={faCopy }  />&nbsp;&nbsp; Copy to Clipboard</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     <button> <FontAwesomeIcon className='text-[gray]' icon={faCopy }  />Copy to Clipboard</button>
+     </center>
+
+   
+
+          
+
       <label htmlFor="length">{length}</label>
       <input type="range" id="length" min={1} max={30} onChange={(e) => setLength(e.target.value)} value={length} />
 
