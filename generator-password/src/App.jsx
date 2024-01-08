0056f121fc92eb9be528b31a37792d85464b82c0
umair kifayat <umairkifayat3@gmail.com>
2024-01-07 21:31:24 -0800

    'completed'

--- generator-password/src/App.jsx
+++ generator-password/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCopy } from '@fortawesome/free-solid-svg-icons';
+import { faCopy, faSync } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   const [password, setPassword] = useState('');
@@ -16,6 +16,7 @@ function App() {
     setNumber(e.target.checked);
   }
 
+
   function includeSymbol(e) {
     setSymbol(e.target.checked);
   }
@@ -24,16 +25,19 @@ function App() {
     navigator.clipboard.writeText(password)
   }
 
+  const RefreshButton = () => {
+    const handleRefresh = () => {
+      window.location.reload();
+    };
 
-
-
+  }
   function generatePassword() {
     let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
     if (number) {
       str += '1234567890';
     }
     if (symbol) {
-      str += '~!@#$%^&*()_+/*-+?><:"}{\\|[]'; // Corrected the escape character before the backslash
+      str += '~!@#$%^&*()_+/*-+?><:"}{\\|[]';
     }
     let generatedPassword = '';
     for (let i = 0; i < length; i++) {
@@ -48,31 +52,38 @@ function App() {
   return (
 
     <>
+    <h1 className='mt-3 text-[#020f66] p-5 text-[50px] font-mono'>Password Generator</h1>
    <center >
-                <h1 className=' font-serif text-5xl mt-[6%] bg-[#e6ebef] text-[#175ddc] p-[4%]
-   w-[80%]  rounded-[15px]'>{password}</h1>
+                <h1 className=' font-serif text-5xl mt-[3%] bg-[#e6ebef] text-[#175ddc] p-[4%]
+   w-[80%]  rounded-[15px] whitespace-pre'>{password}</h1>
                 </center>
 
          <center>             
-     <button onClick={ copyText} className='bg-[#020f66] text-white mt-5 p-[1%] rounded-[15px]'><FontAwesomeIcon className='text-[gray] rounded-2xl' icon={faCopy }  />&nbsp;&nbsp; Copy to Clipboard</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-     <button> <FontAwesomeIcon className='text-[gray]' icon={faCopy }  />Copy to Clipboard</button>
+     <button onClick={ copyText} className='bg-[#020f66] text-white mt-5 p-[1%] rounded-[15px] w-[30%]'><FontAwesomeIcon className='text-[white] rounded-2xl' icon={faCopy }  />&nbsp;&nbsp; Copy to Clipboard</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     <button onClick={generatePassword} className='bg-[#175ddc] hover:bg-[#020f66]  text-white mt-5 p-[1%] rounded-[15px]  w-[30%]' ><FontAwesomeIcon icon={faSync} className='text-[white] rounded-2xl' />&nbsp;Generate</button>
      </center>
 
    
-
-          
-
-      <label htmlFor="length">{length}</label>
-      <input type="range" id="length" min={1} max={30} onChange={(e) => setLength(e.target.value)} value={length} />
-
-      <div>
-        <label htmlFor="number">Number</label>
-        <input type="checkbox" id="number" onChange={includeNumber} checked={number} />
-      </div>
-      <div>
+<center >
+       <div className='flex justify-evenly bg-[#e6ebef] p-[4%] rounded-[15px] w-[80%] mt-5' >
+        <div >
+                  <label htmlFor="number" >Number</label>
+        <input type="checkbox" id="number" onChange={includeNumber} checked={number} /><br />
+  
         <label htmlFor="symbol">Symbol</label>
-        <input type="checkbox" id="symbol" onChange={includeSymbol} checked={symbol} />
+        <input type="checkbox" id="symbol" onChange={includeSymbol} checked={symbol} /><br />
+        </div>
+
+       <div>
+
+       <label htmlFor="length">Characters:{length}</label><br />
+      
+ 
+      <input type="range" id="length"  min={1} max={30} onChange={(e) => setLength(e.target.value)} value={length} />
       </div>
+      </div> 
+      </center>
+     
     </>
   );
 }
